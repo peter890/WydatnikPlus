c538cd8dde667f1819950111835dc36a986d2972
peter890 <zolter17@gmail.com>
2012-04-15 11:15:09 +0200

    implementacja funkcji addProduct()

--- WydatnikPlus/src/com/wydatnik/plus/ShoppingList.java
+++ WydatnikPlus/src/com/wydatnik/plus/ShoppingList.java
@@ -1,12 +1,21 @@
 package com.wydatnik.plus;
 
-public abstract class ShoppingList {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public  class ShoppingList {
 
 	/**
 	 * dodaje otrzymany przez parametr produkt do(listy zakupów) bazy danych
+	 * @throws SQLException s
 	 */
-	public void addProduct(Product produkt) {
-	}
+	public void addProduct(Product produkt) throws SQLException {
+	StringBuffer sqlQuery = new StringBuffer();
+	sqlQuery.append("INSERT INTO \"main\".\"pio_tran\" (\"title\",\"amount\",\"type\",\"data\") \"VALUES \"("+produkt.getNazwa()+","+produkt.getCena()+","+produkt.getTyp()+","+produkt.getData()+")");
+	Connection cn = MysqlConnection.getConnection();
+	ResultSet rs = cn.createStatement().executeQuery(sqlQuery.toString());
+	} 
 
 	/**
 				 */
@@ -17,11 +26,12 @@ public abstract class ShoppingList {
 					 */
 	public void moveToTheExpenditure(Product produkt) {
 	}
-
 		
 		/**
 		 */
-		public abstract void show();
+		//public abstract void show();
 		
-
+public static void main(String[] arg){
+	ShoppingList sl = new ShoppingList();
+}
 }
